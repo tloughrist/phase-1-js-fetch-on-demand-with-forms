ea35d4b92b02a30185f7293b843762183c589c89
timloughrist <tim.loughrist@gmail.com>
2022-08-18 09:46:54 -0500

    commit message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //console.log(e);
+    const input = e.target.children[1].value;
+    
+    console.log(input);
+
+    fetch(`http://localhost:3000/movies/${input}`)
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data);
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    })
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
